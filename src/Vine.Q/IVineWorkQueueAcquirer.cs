4e9e6c11b7a9980cb137d1b965eb258e0c29f741
wentie song <ellefry@live.cn>
2024-01-02 16:16:27 +0800

    queue builder refactoring

--- src/Vine.Q/IVineWorkQueueAcquirer.cs
+++ src/Vine.Q/IVineWorkQueueAcquirer.cs
@@ -1,8 +1,25 @@
-﻿namespace Vine.Q;
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Vine.Q;
 
 public interface IVineWorkQueueAcquirer
 {
     IVineWorkQueue<T>? GetWorkQueue<T>(string name);
 }
 
+public class VineWorkQueueAcquirer : IVineWorkQueueAcquirer
+{
+    private readonly IEnumerable<IVineWorkQueue> _vineWorkQueues;
+
+    public VineWorkQueueAcquirer(IEnumerable<IVineWorkQueue> vineWorkQueues)
+    {
+        _vineWorkQueues = vineWorkQueues;
+    }
+
+    public IVineWorkQueue<T>? GetWorkQueue<T>(string name)
+    {
+        return _vineWorkQueues.FirstOrDefault(x => x.Name == name) as IVineWorkQueue<T>;
+    }
+}
 
--- src/Vine.Q/VineQueueBuilder.cs
+++ src/Vine.Q/VineQueueBuilder.cs
@@ -3,36 +3,26 @@ using System.Collections.Concurrent;
 
 namespace Vine.Q;
 
-public class VineQueueBuilder : IVineQueueBuilder, IVineWorkQueueAcquirer
+public class VineQueueBuilder : IVineQueueBuilder
 {
-    private static readonly ConcurrentDictionary<string, IVineWorkQueue> _dictionary = new();
-
     public static VineWorkQueue<T> Create<T>(string name, int capacity)
     {
         var queue = new VineWorkQueue<T>(name, capacity);
-        _dictionary.TryAdd(name, queue);
         return queue;
     }
 
-    public void Create<T>(string name, int capacity, Action<T> onNext)
+    public IVineWorkQueue Create<T>(string name, int capacity, Action<T> onNext)
     {
         var queue = Create<T>(name, capacity);
         queue.RegisterHandler(onNext);
+        return queue;
     }
 
-    public void Create<T, TReturn>(string name, int capacity, Func<T, TReturn> onNext)
+    public IVineWorkQueue Create<T, TReturn>(string name, int capacity, Func<T, TReturn> onNext)
     {
         var queue = Create<T>(name, capacity);
         queue.RegisterHandler(onNext);
-    }
-
-    public IVineWorkQueue<T>? GetWorkQueue<T>(string name)
-    {
-        if (_dictionary.TryGetValue(name, out var workQueue) && workQueue is IVineWorkQueue<T> wq)
-        {
-            return wq;
-        }
-        return null;
+        return queue;
     }
 }
 
